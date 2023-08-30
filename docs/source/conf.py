0f206e92427c6d175671c5236ccb9c958c67de82
Mchoi-git <matthew.choi@v.vectorinstitute.ai>
2023-08-30 15:51:12 -0400

    built docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,9 +6,6 @@
 import os
 import sys
 
-import sphinx_rtd_theme
-
-
 sys.path.insert(0, os.path.abspath("../../flex_model"))
 
 # -- Project information -----------------------------------------------------
@@ -33,5 +30,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_path=[sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
